80b8e92b154eb1f822d0cb8847a093adb11fbcee
Matheus <matheusmhs@hotmail.com>
2020-11-27 18:39:10 -0300

    Trying syntax tree

--- PalomaMatheus/Aplicação/main.py
+++ PalomaMatheus/Aplicação/main.py
@@ -1,14 +1,8 @@
 from menu import Menu
 
-from conversoes import *
-from GR import RegularGrammar
-from ER import RegularExpression
-from AF import *
-
-
 def main():
 	menu = Menu()
 	menu.start()
 
 if __name__ == "__main__":
-    main()
+	main()
